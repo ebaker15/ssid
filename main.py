0d5ef1a85c9568aec55ac6437f601484a2a9efa9
facedesk <dgold@sjnma.org>
2017-07-05 10:56:47 -0500

    got password exported successfully using netsh

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
 from subprocess import call
+from os import listdir
+from os.path import isfile, join
 
 words = [
 "fieljazair#99"]
 
 def try_connect(ssid):
     for word in words:
-        connectone(ssid,word)
-def connectone(ssid,password):
-    setnetwork = "netsh wlan set hostednetwork mode=allow ssid={0} key={1}".format(ssid,password)
+        _connectone(ssid,word)
+def _findxml():
+    mypath = "./"
 
-    connect = "netsh wlan connect name="+ ssid
+    onlyxmlfiles = [f for f in listdir(mypath) if (isfile(join(mypath, f))and f.lower().endswith(".xml"))]
+    if(len(onlyxmlfiles)== 1):
+        return onlyxmlfiles[0]
+    else:
+        return "yourxml was not found!"
+def _replacepassword(password):
+    pass
+def _connectone(ssid,password):
+    exportprofile='netsh wlan export profile name="{0}" folder= "{1}" key=clear '.format(ssid,".")
+
+    xmlfile = _findxml()
+    addnetwork = 'netsh wlan add profile filename="'+xmlfile+'"'
+
+    removenetwork = 'netsh wlan delete profile name="'+ssid+'"'
+    setnetwork = 'netsh wlan set hostednetwork mode=allow ssid="{0}" key="{1}"'.format(ssid,password)
+    connect = 'netsh wlan connect name="'+ ssid + '"'
+
+    print (exportprofile)
+    call (exportprofile)
+
+'''
+    #call(removenetwork)
     call(setnetwork)
-
+    print(setnetwork)
     result = call(connect)
-    print result
-
+    print (result)
+'''
 
 
 try_connect("Linksys05289")
