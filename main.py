18c040ae7a0820b0929a3c206299bcaba16e921e
facedesk <dgold@sjnma.org>
2017-07-14 08:28:10 -0500

    beta release

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
 from subprocess import call
+from subprocess import check_output
 from os import listdir
 from os.path import isfile, join
+import xmltodict
+from dicttoxml import dicttoxml
+from subprocess import Popen, PIPE
+
+import time
 
 words = [
-"fieljazair#99"]
+"fieljazair#99"
+]
 
-def try_connect(ssid):
+def try_connect(ssid, verbose = False):
     for word in words:
-        _connectone(ssid,word)
+        if(verbose):
+            print("Trying " + word)
+        if(_connectone(ssid,word,1,verbose)):
+            return word
+    return "no ssid found"
+import shlex
+from subprocess import Popen, PIPE
+
+def get_exitcode_stdout_stderr(cmd):
+    """
+    Execute the external command and get its exitcode, stdout and stderr.
+    """
+    args = shlex.split(cmd)
+
+    proc = Popen(args, stdout=PIPE, stderr=PIPE)
+    out, err = proc.communicate()
+    exitcode = proc.returncode
+    #
+    return exitcode, out, err
+def test_on_network(ssid,verbose=False):
+    exitcode,output,error = get_exitcode_stdout_stderr("ping google.com")
+    #output = str(check_output("netsh wlan show interfaces"))
+    output = str(output)
+    if(verbose):
+        print(output.replace('\\r\\n','\r\n'))
+    return not "could not find host" in output
 def _findxml():
     mypath = "./"
 
@@ -16,28 +48,38 @@ def _findxml():
         return onlyxmlfiles[0]
     else:
         return "yourxml was not found!"
-def _replacepassword(password):
-    pass
-def _connectone(ssid,password):
-    exportprofile='netsh wlan export profile name="{0}" folder= "{1}" key=clear '.format(ssid,".")
+def _replacepassword(password,xmlfile):
+    with open(xmlfile) as fd:
+        doc = xmltodict.parse(fd.read())
+    doc['WLANProfile']['MSM']['security']['sharedKey']['keyMaterial'] = password
 
+    xml = xmltodict.unparse(doc, pretty=True)
+    f = open(xmlfile, 'w')
+    f.write(str(xml))
+    f.close()
+
+def _connectone(ssid,password, timetosleep=5,verbose=False):
+    exportprofile='netsh wlan export profile name="{0}" folder= "{1}" key=clear '.format(ssid,".")
+    call (exportprofile)
     xmlfile = _findxml()
-    addnetwork = 'netsh wlan add profile filename="'+xmlfile+'"'
+    _replacepassword(password,xmlfile)
 
     removenetwork = 'netsh wlan delete profile name="'+ssid+'"'
-    setnetwork = 'netsh wlan set hostednetwork mode=allow ssid="{0}" key="{1}"'.format(ssid,password)
+    call (removenetwork)
+
+    addnetwork = 'netsh wlan add profile filename="'+xmlfile+'"'
+    call (addnetwork)
+
+    #setnetwork = 'netsh wlan set hostednetwork mode=allow ssid="{0}" key="{1}"'.format(ssid,password)
+    #call (setnetwork)
+
     connect = 'netsh wlan connect name="'+ ssid + '"'
+    call(connect)
 
-    print (exportprofile)
-    call (exportprofile)
+    time.sleep(timetosleep)
+    connected = test_on_network(ssid,True)
+    print(connected)
+    return connected
 
-'''
-    #call(removenetwork)
-    call(setnetwork)
-    print(setnetwork)
-    result = call(connect)
-    print (result)
-'''
-
-
-try_connect("Linksys05289")
+print("Searching for password\n...\n...\n...")
+print(try_connect("Linksys05289",verbose=True))
--- test.py
+++
@@ -1,5 +0,0 @@
-test = 0
-for x in range(999999):
-    test = x#
-    #print(test)
-print (test)
